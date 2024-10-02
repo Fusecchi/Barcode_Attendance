23c850c4c00cc7dfbc16451fa893c508049c12dd
PC <andi.fytra@gmail.com>
2024-10-02 09:13:10 +0700

    First Push

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,93 +1,84 @@
 package com.example.myapplication
 
-
-import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.VerticalDivider
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMainBinding
 import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
 import com.journeyapps.barcodescanner.ScanOptions
 
-class MainActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MyApplicationTheme {
-                Scaffold (
-                    bottomBar = {
-                        BottomAppBar (
-                            actions = {},
-                            floatingActionButton =  {
-                                Icon(
-                                    painter = painterResource(id = R.drawable.qr_scan),
-                                    contentDescription = "QR Scan"
-                                )
-                            }
-                        )
-                    }
-                ){
-                     innerPadding->   Column (
-                         modifier = Modifier
-                             .fillMaxSize()
-                             .padding(),
-                         verticalArrangement = Arrangement.Center,
-                         horizontalAlignment = Alignment.CenterHorizontally
-                     ){
-                    Image(
-                        painter = painterResource(id = R.drawable.qr_scan),
-                        modifier = Modifier.size(100.dp),
-                        contentDescription =  "QR"
+class MainActivity : AppCompatActivity() {
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            isGranted: Boolean ->
+            if (isGranted){
+                showCamera()
+            }else{
 
-                    )
-                }
+            }
+        }
+    private val scanLauncher =
+        registerForActivityResult(ScanContract()){
+            result: ScanIntentResult ->
+            run {
+                if (result.contents == null) {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
+                } else {
+                    setResult(result.contents)
                 }
             }
         }
+    private lateinit var binding: ActivityMainBinding
+
+    private fun setResult(string: String){
+        binding.textResult.text = string
     }
-}
 
-    private var textresult = mutableStateOf("")
+    private fun showCamera() {
+        val options = ScanOptions()
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+        options.setPrompt("Scan QR Code")
+        options.setCameraId(0)
+        options.setBeepEnabled(false)
+        options.setBarcodeImageEnabled(true)
+        options.setOrientationLocked(false)
 
-    private val barCodeLauncher = registerForActivityResult(ScanContract()){ result ->
-        if (result.contents == null) {
-            Toast.makeText(this@MainActivity, "Cancelled", Toast.LENGTH_SHORT).show()
-        }else{
-            textresult.value = result.contents
+        scanLauncher.launch(options)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initBinding()
+        initViews()
+        }
+
+    private fun initViews() {
+        binding.fab.setOnClickListener{
+            checkPermissionCamera(this)
         }
     }
 
-    private fun showCamera(){
-        val options = ScanOptions()
-        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
-        options.setPrompt("scan a QR Code")
-        options.setCameraId(0)
-        options.setBeepEnabled(false)
-        options.setOrientationLocked(false)
-
-        barCodeLauncher.la
+    private fun checkPermissionCamera(context: Context) {
+        if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+            showCamera()
+        }else if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)){
+            Toast.makeText(context, "Camera Permission Required", Toast.LENGTH_SHORT).show()
+        }else{
+            requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
+        }
     }
+
+    private fun initBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
+
